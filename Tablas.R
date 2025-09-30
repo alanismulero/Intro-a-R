2a4c9dd6de9a269842a25e448f7905e959b5dcc8
Alanis Mulero <alanis.mulero@upr.edu>
2025-09-30 16:35:52 -0400

    c1

---
+++ Tablas.R
@@ -0,0 +1,7 @@
+
+# Tablas ------------------------------------------------------------------
+
+install.packages('knitr')
+install.packages("gt")
+install.packages("kableExtra")
+install.packages("gaawr2")
